6d7342ef5e2f0c79c74b470652696f985dd83ba7
Lee-Jinyong <yjy1480@gmail.com>
2024-07-23 16:55:21 +0900

    [학습] 웹 컴포넌트 활용 (재사용성, 유지보수성)

--- 02-design-handoff/src/step-03.js
+++ 02-design-handoff/src/step-03.js
@@ -1,91 +1,117 @@
-// [X] STEP 1. Vanilla Script (ES + DOM API)
+// ---------------------------------------------------------
+// [ ] STEP 1. Vanilla Script (ES + DOM API)
 // [ ] STEP 2. Class Programming
-// [ ] STEP 3. Web Components API
+// [x] STEP 3. Web Components API
+// ---------------------------------------------------------
 
-// TODO: 드래깅 상태 제어를 위한 상수 선언
-const DRAGGING_CLASSNAME = "dragging";
+// Custom Element
+// Shadow DOM (encapsulation)
+// Templates & Slots
+// Lifecycle methods
+// - connectedCallback (mounted)
+// - disconnectedCallback (unmounted)
 
-// TODO: .list 요소 찾기
-const list = document.querySelector(".list");
+class SortableList extends HTMLElement {
+  // 클래스 멤버
+  static DRAGGING_CLASSNAME = "dragging";
 
-// TODO: list 드래그 이벤트 핸들링
-list.addEventListener("dragover", (e) => {
-  // 브라우저 기본 작동 중지
-  e.preventDefault();
+  // 프라이베이트 멤버
+  #list = null;
+  #items = [];
 
-  // TODO: 현재 드래깅 중인 아이템 찾기
-  const draggedItem = listItems.find((item) =>
-    item.classList.contains(DRAGGING_CLASSNAME)
-  );
+  #init() {
+    console.log("초기화");
 
-  // TODO: 드래깅 중인 요소가 아닌 나머지 아이템 집합 찾기
-  const restItems = listItems.filter((item) => !Object.is(item, draggedItem));
+    // 구조 분해 할당
+    const { DRAGGING_CLASSNAME } = SortableList;
 
-  // TODO: 나머지 아이템 중에 드래깅 요소가 드래깅 중인 화면 상의 높이와
-  //       드롭 대상 요소의 화면에서의 top 위치 + (높이 * 0.5) 비교해서 교체할 아이템 찾기
+    const list = this.#list;
+    const items = this.#items;
 
-  // 드래깅 중인 요소가 리스트 안에서 움직일 때 화면에서의 높이 값 (이벤트 감지)
-  // console.log(e.clientY);
+    // 이벤트 바인딩(연결)
 
-  const replaceItem = restItems.find((item, index) => {
-    // console.log(index, item.offsetTop);
-    // console.log(index, item.offsetHeight);
-    // console.log(index, item.offsetHeight * 0.5);
-    return e.clientY <= item.offsetTop + item.offsetHeight * 0.5;
-  });
+    // 1. 리스트 이벤트 핸들링
+    list.addEventListener("dragover", (e) => {
+      e.preventDefault();
 
-  if (replaceItem) {
-    list.insertBefore(draggedItem, replaceItem);
-  } else {
-    list.appendChild(draggedItem);
-  }
-});
+      const draggedItem = items.find((item) =>
+        item.classList.contains(DRAGGING_CLASSNAME)
+      );
 
-// TODO: .list 자식들(children, 집합) 찾기
-// let listItems = list.querySelectorAll("li"); // NodeList
-// listItems = Array.from(listItems); // NodeList -> Array
-const listItems = Array.from(list.querySelectorAll("li"));
+      const restItems = items.filter((item) => !Object.is(item, draggedItem));
 
-// TODO: listItems 집합 순환 드래그 가능하게 처리
-listItems.forEach((item) => {
-  item.setAttribute("draggable", true);
-  // item.setAttribute('tabindex', 0);
-  // item.style.cursor = 'move';
+      const replaceItem = restItems.find((item, index) => {
+        return e.clientY <= item.offsetTop + item.offsetHeight * 0.5;
+      });
 
-  // TODO: 각 리스트 아이템에 드래그 시작, 끝 이벤트 핸들링
-  item.addEventListener("dragstart", (e) => {
-    // console.log(e.type, e.currentTarget);
-    e.currentTarget.classList.add(DRAGGING_CLASSNAME);
-  });
+      list.insertBefore(draggedItem, replaceItem);
+    });
 
-  item.addEventListener("dragend", (e) => {
-    // console.log(e.type, e.currentTarget);
-    e.currentTarget.classList.remove(DRAGGING_CLASSNAME);
-  });
+    // 2. 리스트 아이템 순환, 각 아이템 이벤트 핸들링
+    items.forEach((item) => {
+      item.setAttribute("draggable", true);
 
-  const handleButton = item.querySelector('[data-role="handle"]');
-  if (handleButton) {
-    handleButton.addEventListener("keyup", (e) => {
-      switch (e.key) {
-        case "ArrowUp":
-          // case "ArrowLeft":
-          const prevItem = item.previousElementSibling;
-          if (prevItem) {
-            prevItem.before(item, prevItem);
-            // 핸들에게 초점 설정(유지)
-            e.currentTarget.focus();
+      item.addEventListener("dragstart", (e) => {
+        e.currentTarget.classList.add(DRAGGING_CLASSNAME);
+      });
+
+      item.addEventListener("dragend", (e) => {
+        e.currentTarget.classList.remove(DRAGGING_CLASSNAME);
+      });
+
+      const handleButton = item.querySelector('[data-role="handle"]');
+
+      if (handleButton) {
+        handleButton.addEventListener("keyup", (e) => {
+          switch (e.key) {
+            case "ArrowUp":
+              const prevItem = item.previousElementSibling;
+              if (prevItem) {
+                prevItem.before(item);
+
+                e.currentTarget.focus();
+              }
+
+              break;
+
+            case "ArrowDown":
+              const nextItem = item.nextElementSibling;
+              if (nextItem) {
+                nextItem.after(item);
+
+                e.currentTarget.focus();
+              }
+
+              break;
           }
-          break;
-        case "ArrowDown":
-          // case "ArrowRight":
-          const nextItem = item.nextElementSibling;
-          if (nextItem) {
-            nextItem.after(item);
-            // 핸들에게 초점 설정(유지)
-            e.currentTarget.focus();
-          }
-          break;
+        });
       }
     });
   }
-});
+
+  // constructor() {
+  //   super()
+  // }
+
+  connectedCallback() {
+    this.#items = Array.from(this.children);
+    // console.log(this.#items);
+
+    this.innerHTML = /* html */ `
+      <ul class="list"></ul>
+    `;
+
+    this.#list = this.querySelector(".list");
+
+    // 전개 구문
+    this.#list.append(...this.#items);
+
+    this.#init();
+  }
+
+  disconnectedCallback() {
+    console.log("unmounted");
+  }
+}
+
+customElements.define("sortable-list", SortableList);
