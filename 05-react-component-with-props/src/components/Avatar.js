1b5c5ed5d1e44c69af3b13ba56b5060326a3a283
Lee-Jinyong <yjy1480@gmail.com>
2024-07-26 14:38:09 +0900

    [실습] Avatar 컴포넌트 마크업 Part. 1

---
+++ 05-react-component-with-props/src/components/Avatar.js
@@ -0,0 +1,43 @@
+import { createElement as h } from "https://esm.sh/react";
+
+/* 
+<figure aria-label="이름 (온라인)">
+  <img src="face.jpg" alt="이름" />
+  <figcaption>
+    <img />
+  </figcaption>
+</figure>
+*/
+
+/* props: {
+  name: '사용자 이름',
+  photo: '이미지 파일 이름',
+  status?: 'online' | 'offline' | 'dont-disturb' | 'away'
+  size?: 숫자 값
+} */
+
+function Avatar({ name, photo, status = "offline", size = 64 }) {
+  return h(
+    "figure",
+    {
+      className: "Avatar",
+      "aria-label": `${name} (상태)`,
+    },
+    h("img", {
+      src: `/faces/${photo}`,
+      alt: name,
+      width: size,
+      height: size,
+    }),
+    h(
+      "figcaption",
+      null,
+      h("img", {
+        src: "/icons/status-offline.svg",
+        alt: "",
+      })
+    )
+  );
+}
+
+export default Avatar;
