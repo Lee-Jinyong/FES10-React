6ef11616b96f2d36f87a1f3ab52602fc40ff1174
Lee-Jinyong <yjy1480@gmail.com>
2024-07-26 14:45:57 +0900

    [실습] Avatar 컴포넌트 마크업 Part. 2
    - switch 로직 활용

--- 05-react-component-with-props/src/pages/AvatarListPage.js
+++ 05-react-component-with-props/src/pages/AvatarListPage.js
@@ -7,8 +7,21 @@ function AvatarListPage() {
     {
       className: "AvatarList",
     },
-    h("li", null, h(Avatar, { name: "야무", photo: "man-02.jpg" })),
-    h("li", null, h(Avatar, { name: "범쌤", photo: "man-03.jpg" }))
+    h(
+      "li",
+      null,
+      h(Avatar, { name: "야무", photo: "man-02.jpg", status: "online" })
+    ),
+    h(
+      "li",
+      null,
+      h(Avatar, {
+        name: "범쌤",
+        photo: "man-04.jpg",
+        size: 128,
+        status: "away",
+      })
+    )
   );
 }
 
