040294672675f9431d3b6c27a125b147ae93dc24
Lee-Jinyong <yjy1480@gmail.com>
2024-07-30 10:20:58 +0900

    [학습] JSX 마크업 Part 3.
    - 데이터 바인딩(보간: 사이를 채우다)
    - 유틸리티 함수 randomNumber

--- 08-practice-make-components/src/learn/data-binding.jsx
+++ 08-practice-make-components/src/learn/data-binding.jsx
@@ -1,9 +1,20 @@
-function DataBinding(props) {
+import { randomNumber } from '../utils';
+
+function DataBinding({ statusMessages }) {
   // <div> 남용
   // 요소를 선택하는 최후의 수단(기준)
   // 1. HTML에는 이미 의미적인 요소가 준비되어 있음
   // 2. 의미에 맞는 요소가 존재하지 않을 때 비로소 <div> 사용 권장
 
+  // data(props)
+  // random logic을 작성해서 임의의 상태 메시지가 표시되도록 하기
+  // JavaScript 프로그래밍
+  // Math.random() / Math.floor() / Math.round()
+
+  const statusMessage =
+    statusMessages[randomNumber(0, statusMessages.length - 1)];
+
+  // JSX (generate markup)
   return (
     <>
       <dt>데이터 바인딩(data binding)</dt>
@@ -11,6 +22,7 @@ function DataBinding(props) {
         <p>상태 메시지(status message)를 연결해 화면에 출력합니다.</p>
         <span className="status">
           {/* statusMessage 값을 화면에 표시합니다. (랜덤 표시도 도전!) */}
+          {statusMessage}
         </span>
       </dd>
     </>
