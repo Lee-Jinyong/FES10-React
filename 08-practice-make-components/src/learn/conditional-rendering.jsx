f78c18aa1640b39ab78675b835cf20fc96d6fa6f
Lee-Jinyong <yjy1480@gmail.com>
2024-07-30 15:08:56 +0900

    [학습] JSX 마크업 Part 4.3. 조건부 렌더링
    - if문 -> switch문 변경

--- 08-practice-make-components/src/learn/conditional-rendering.jsx
+++ 08-practice-make-components/src/learn/conditional-rendering.jsx
@@ -16,22 +16,46 @@ function ConditionalRendering({ imageType }) {
   // 조건에 따라 렌더링 결과가 달라짐
   let imagePath = '';
   let printText = '';
-  if (imageType.toLowerCase().includes('react')) {
-    imagePath = reactImagePath;
-    printText = 'React';
+
+  // 조건"문"
+  // if 문 => switch 문으로 변경
+  switch (imageType.toLowerCase()) {
+    case 'react':
+      imagePath = reactImagePath;
+      printText = 'React';
+      break;
+    case 'vite':
+      imagePath = viteImagePath;
+      printText = 'Vite';
+      break;
+    case 'next.js':
+      imagePath = nextJsImagePath;
+      printText = 'Next.js';
+      break;
+    case 'kakao talk':
+      imagePath = kakaoTalkImagePath;
+      printText = 'Kakao Talk';
+      break;
+    default:
+      printText = '허용된 이미지 타입이 존재하지 않습니다.';
   }
-  if (imageType.toLowerCase().includes('vite')) {
-    imagePath = viteImagePath;
-    printText = 'Vite';
-  }
-  if (imageType.toLowerCase().includes('next.js')) {
-    imagePath = nextJsImagePath;
-    printText = 'Next.js';
-  }
-  if (imageType.toLowerCase().includes('kakao talk')) {
-    imagePath = kakaoTalkImagePath;
-    printText = 'kakao talk';
-  }
+
+  // if (imageType.toLowerCase().includes('react')) {
+  //   imagePath = reactImagePath;
+  //   printText = 'React';
+  // }
+  // if (imageType.toLowerCase().includes('vite')) {
+  //   imagePath = viteImagePath;
+  //   printText = 'Vite';
+  // }
+  // if (imageType.toLowerCase().includes('next.js')) {
+  //   imagePath = nextJsImagePath;
+  //   printText = 'Next.js';
+  // }
+  // if (imageType.toLowerCase().includes('kakao talk')) {
+  //   imagePath = kakaoTalkImagePath;
+  //   printText = 'Kakao Talk';
+  // }
 
   // 값을 반환하는 함수 실행
   // const isShowSpinner = isTrueOrFalse(); // True Or False
