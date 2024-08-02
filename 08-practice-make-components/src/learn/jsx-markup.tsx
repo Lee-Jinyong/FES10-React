dc42d6ca9c54c3c12242c1abf11e207322f0402b
Lee-Jinyong <yjy1480@gmail.com>
2024-08-02 09:10:34 +0900

    [구성] 절대 경로 사용 설정 (Vite with TypeScript)

--- 08-practice-make-components/src/learn/jsx-markup.tsx
+++ 08-practice-make-components/src/learn/jsx-markup.tsx
@@ -2,7 +2,7 @@ import DataBinding from './data-binding';
 import ConditionalRendering from './conditional-rendering';
 import ConditionalDisplay from './conditional-display';
 import RenderLists from './render-lists';
-import * as learnData from '../data/learn';
+import * as learnData from '@/data/learn';
 
 function JSX_Markup(): JSX.Element {
   let {
