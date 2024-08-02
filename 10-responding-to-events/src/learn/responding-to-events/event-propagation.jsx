037981b6a3d6717eafddf097390ee79a8e913522
Lee-Jinyong <yjy1480@gmail.com>
2024-08-02 15:29:09 +0900

    [학습] 이벤트 전파 II
    - 전파 대안으로 핸들러 prop 전달
    - 하위 컴포넌트에서 가로채 실행
    - 필요한 경우, 이벤트 객체 전달

--- 10-responding-to-events/src/learn/responding-to-events/event-propagation.jsx
+++ 10-responding-to-events/src/learn/responding-to-events/event-propagation.jsx
@@ -1,16 +1,17 @@
 // --------------------------------------------------------------------------
 // ✅ 이벤트 전파
 // --------------------------------------------------------------------------
-// - [ ] 전파 중지
-// - [ ] 전파 대안으로 핸들러 전달
+// - [x] 전파 중지 (개별적으로 할 수 있음)
+// - [x] 전파 대안으로 핸들러 전달
 // - [ ] 기본 작동 방지
 // --------------------------------------------------------------------------
+
+import LayoutBox from './LayoutBox';
 
 // RGB 모드
 // CMYK 모드
 
 // event.preventDefault(); // 이벤트 기본 작동을 차단
-
 // Event Delegation (위임)
 // Event Propagation (전파)
 // event.stopPropagation(); // 이벤트 전파 중지
@@ -22,35 +23,36 @@ function EventPropagation() {
         <b>이벤트 전파 &amp; 기본 작동 방지</b>
       </summary>
       {/* 상위 컴포넌트 : 정민 */}
-      <div
+      <LayoutBox
+        style={styles.cyan}
         onClick={(e) => {
           console.log('cyan', e.target);
         }}
-        className="box"
-        style={styles.cyan}
       >
-        {/* 하위 컴포넌트 : 동호 */}
-        <div
+        <LayoutBox
+          style={styles.magenta}
           onClick={(e) => {
             console.log('magenta', e.target);
           }}
-          className="box"
-          style={styles.magenta}
         >
-          {/* 자손 컴포넌트 : 재명 */}
-          <div
+          <LayoutBox
+            style={styles.yellow}
             onClick={(e) => {
               console.log('yellow', e.target);
             }}
-            className="box"
-            style={styles.yellow}
-          ></div>
-        </div>
-      </div>
+          >
+            <LayoutBox
+              style={styles.purple}
+              onClick={(e) => {
+                console.log('purple', e.target);
+              }}
+            ></LayoutBox>
+          </LayoutBox>
+        </LayoutBox>
+      </LayoutBox>
     </details>
   );
 }
-
 const styles = {
   cyan: {
     '--color': 'var(--cyan)',
@@ -60,6 +62,9 @@ const styles = {
   },
   yellow: {
     '--color': 'var(--yellow)',
+  },
+  purple: {
+    '--color': 'var(--purple, #7423f6)',
   },
 };
 
